a3f67ccb2af6fc166418950fbb6351a50238f81c
Fabien GIBON <fabien.gibon@yahoo.fr>
2022-04-17 23:49:04 +0200

    Ajout de la page réservation

---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ReservationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReservationController extends AbstractController
+{
+  #[Route('/reservation', name: 'app_reservation')]
+  public function index(): Response
+  {
+    $form = $this->createForm(ReservationType::class);
+    return $this->renderForm('reservation/index.html.twig', [
+      'controller_name' => 'ReservationController',
+      'reservation' => $form
+    ]);
+  }
+}
