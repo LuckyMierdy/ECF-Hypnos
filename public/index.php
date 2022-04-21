be494aa7e1bb9414f1f3d3f98ee216aa3f6b0a5b
Fabien GIBON <fabien.gibon@yahoo.fr>
2022-04-21 08:41:31 +0200

    .

--- public/index.php
+++ public/index.php
@@ -1,9 +1,21 @@
 <?php
 
+if (getenv('JAWSDB_URL') !== false) {
+  $hostname = $dbparts['host'];
+  $username = $dbparts['user'];
+  $password = $dbparts['pass'];
+  $database = ltrim($dbparts['path'], '/');
+} else {
+  $hostname = 'root';
+  $username = 'Ff9120851!!';
+  $password = 'test';
+  $database = 'localhost';
+}
+
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+  return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
